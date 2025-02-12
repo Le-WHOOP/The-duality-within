d24c6c195aed1233db2fbffdab7a6feb156c1320
Remi TROTEL <remi.trotel@epita.fr>
2025-02-12 10:18:58 +0100

    Added comments

--- Assets/Scripts/Game/PlayerController.cs
+++ Assets/Scripts/Game/PlayerController.cs
@@ -14,11 +14,23 @@ public class PlayerController : MonoBehaviour
         _body = GetComponent<Rigidbody2D>();
     }
 
+    /// <summary>
+    /// Moves the player by setting the linear velocity.
+    /// </summary>
+    /// <remarks>
+    /// Values are relatives to one another.
+    /// (x, y) = (1, 2) will do the same as (x, y) = (2, 4)
+    /// </remarks>
+    /// <param name="x">The horizontal movement input.</param>
+    /// <param name="y">The vertical movement input.</param>
     public void Move(float x, float y)
     {
         _body.linearVelocity = new Vector2(x, y).normalized * _speed;
     }
 
+    /// <summary>
+    /// Attempts to interact with a nearby interactable object, if one is within range.
+    /// </summary>
     public void Interact()
     {
         throw new NotImplementedException();
