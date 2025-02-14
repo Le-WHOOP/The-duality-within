6c890b27216431c8563c4c4ec42bb1cc4160c972
Magali Gilbert <magali.gilbert@epita.fr>
2025-02-14 23:30:46 +0100

    Update: sprites

--- Assets/Editor/SpriteImporter.cs
+++ Assets/Editor/SpriteImporter.cs
@@ -3,6 +3,13 @@ using UnityEngine;
 
 public class SpriteImporter : AssetPostprocessor {
 
+    /// <summary>
+    /// Set the default values for 2d sprites.
+    /// </summary>
+    /// <remarks>
+    /// These will be applied automatically on new Texture2D import.
+    /// </remarks>
+    /// <param name="texture">The imported sprite.</param>
     void OnPostprocessTexture(Texture2D texture)
     {
         TextureImporter textureImporter = (TextureImporter)assetImporter;
@@ -11,7 +18,7 @@ public class SpriteImporter : AssetPostprocessor {
         textureImporter.spritePixelsPerUnit = 32;
         textureImporter.filterMode = FilterMode.Point;
 
-        textureImporter.SetPlatformTextureSettings( new TextureImporterPlatformSettings {
+        textureImporter.SetPlatformTextureSettings(new TextureImporterPlatformSettings {
             maxTextureSize = 2048,
             resizeAlgorithm = TextureResizeAlgorithm.Bilinear,
             textureCompression = TextureImporterCompression.Uncompressed
