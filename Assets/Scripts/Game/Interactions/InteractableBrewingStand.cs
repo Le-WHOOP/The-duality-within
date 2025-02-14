e3ff809b602b3f6eb9379d1b55a24396aca9be90
Pauline CHARASSON <pauline.charasson@epita.fr>
2025-02-14 17:08:23 +0100

    fixed compile error

--- Assets/Scripts/Game/Interactions/InteractableBrewingStand.cs
+++ Assets/Scripts/Game/Interactions/InteractableBrewingStand.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-public class InteractableBrewingStant : InteractableController
+﻿public class InteractableBrewingStant : InteractableController
 {
     public InteractableBrewingStant(Personnality availableTo) : base(Personnality.Jekyll) { }
 
@@ -15,3 +9,4 @@ public class InteractableBrewingStant : InteractableController
             // TODO Finish game
         }
     }
+}
