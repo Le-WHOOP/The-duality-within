73b05e54f0865f547a97542c642289f9d1b0cefd
Abigaëlle Panhelleux <100417910+LunaPak14@users.noreply.github.com>
2025-02-14 16:47:07 +0100

    [menus] finished the menus until the start of the game

--- Assets/Scripts/Menu/MenuController.cs
+++ Assets/Scripts/Menu/MenuController.cs
@@ -1,3 +1,5 @@
+using System;
+using System.Collections;
 using UnityEngine;
 
 public class MenuController : MonoBehaviour
@@ -5,15 +7,32 @@ public class MenuController : MonoBehaviour
     [SerializeField]
     private ScreenController activeScreen;
 
+    [SerializeField]
+    private Animator transition;
+
     void Start()
     {
         activeScreen.gameObject.SetActive(true);
     }
 
-    public void OpenScreen(ScreenController screen)
+    public void OpenScreen(ScreenController screen) {
+        StartCoroutine(LoadScreen(screen));
+    }
+    public IEnumerator LoadScreen(ScreenController screen)
     {
+        // Start fading animation
+        transition.SetTrigger("Start");
+        // Wait a bit
+        yield return new WaitForSeconds(1);
+
+        // Change the screen
         activeScreen.gameObject.SetActive(false);
         activeScreen = screen;
         activeScreen.gameObject.SetActive(true);
+
+        // Start fading animation
+        transition.SetTrigger("End");
+        //Wait a bit
+        yield return new WaitForSeconds(1);
     }
 }
