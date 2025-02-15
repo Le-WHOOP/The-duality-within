7a88845e189657ecaabfc67c2fd59d0b8d500e19
Remi TROTEL <remi.trotel@epita.fr>
2025-02-15 15:43:33 +0100

    Implemented items inside buildings

--- Assets/Scripts/Game/Interactions/InteractableBuilding.cs
+++ Assets/Scripts/Game/Interactions/InteractableBuilding.cs
@@ -1,4 +1,5 @@
-﻿using UnityEngine;
+﻿using System;
+using UnityEngine;
 
 [RequireComponent(typeof(Collider2D))]
 public class InteractableBuilding : InteractableController
@@ -6,15 +7,20 @@ public class InteractableBuilding : InteractableController
     [SerializeField]
     private BuildingInterior _interior;
 
+    public event EventHandler<CityPlayerController> OnEntered;
+    public event EventHandler<CityPlayerController> OnExited;
+
     public InteractableBuilding() : base(Personnality.Both) { }
 
     public override void Interact(CityPlayerController player)
     {
         _interior.Enter(this, player);
+        OnEntered?.Invoke(this, player);
     }
 
     public void Exit(CityPlayerController player)
     {
         player.transform.position = transform.position;
+        OnExited?.Invoke(this, player);
     }
 }
