37bb7d5b98667f416aef711fd9a176420f0b95e8
Magali Gilbert <magali.gilbert@epita.fr>
2025-02-15 17:38:21 +0100

    Add: beginning of character animation implementation

--- Assets/Scripts/Game/PlayerController.cs
+++ Assets/Scripts/Game/PlayerController.cs
@@ -12,10 +12,12 @@ public class PlayerController : MonoBehaviour
     private float _speed = 1;
 
     private Rigidbody2D _body;
+    private Animator _animator;
 
     void Start()
     {
         _body = GetComponent<Rigidbody2D>();
+        _animator = GetComponentInChildren<Animator>();
     }
 
     void OnTriggerEnter2D(Collider2D collision)
@@ -37,7 +39,20 @@ public class PlayerController : MonoBehaviour
     /// <param name="y">The vertical movement input.</param>
     public void Move(float x, float y)
     {
-        _body.linearVelocity = new Vector2(x, y).normalized * _speed;
+        Vector2 movement = new Vector2(x, y).normalized;
+        _body.linearVelocity = movement * _speed;
+
+        // Only set the animation direction is the player is trying to move
+        if (movement != Vector2.zero) {
+            _animator.SetFloat("x", movement.x);
+            _animator.SetFloat("y", movement.y);
+            _animator.SetBool("isWalking", true);
+        }
+        // FIXME: why does this throw a NullReferenceException()?
+        // > maybe it _is_ _animator causing the issue bcs now I'm getting some "Parameter isWalking does not exist"
+        else {
+            _animator.SetBool("isWalking", false);
+        }
     }
 
     /// <summary>
