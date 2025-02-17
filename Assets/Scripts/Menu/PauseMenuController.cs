f5efd6f93366d4710f2ed57379faa46fdf174f98
Abigaëlle Panhelleux <100417910+LunaPak14@users.noreply.github.com>
2025-02-17 10:16:26 +0100

    [menus] finished the menus

--- Assets/Scripts/Menu/PauseMenuController.cs
+++ Assets/Scripts/Menu/PauseMenuController.cs
@@ -24,7 +24,9 @@ public class PauseMenuController : MonoBehaviour
     [SerializeField]
     private Animator transitionPause;
 
-    // Update is called once per frame
+    /// <summary>
+    /// Update is called once per frame
+    /// </summary>
     void Update()
     {
         if (Input.GetKeyDown(KeyCode.Escape)) {
@@ -36,43 +38,104 @@ public class PauseMenuController : MonoBehaviour
         }
     }
 
+    /// <summary>
+    /// Resume the game
+    /// </summary>
+    /// <remarks>
+    /// It has only one line because a button cannot have 
+    /// an onclick function that returns an IEnumerator
+    /// </remarks>
     public void Resume() 
     {
-        StartCoroutine(MenuAnimation(transitionPause));
-
-        pauseMenu.SetActive(false);
-        Time.timeScale = 1f;
-        isPaused = false;
+        StartCoroutine(PauseMenuAnimation());
     }
 
-    void Pause()
+    /// <summary>
+    /// Pause the game
+    /// </summary>
+    public void Pause()
     {
         pauseMenu.SetActive(true);
+
+        // Time.timeScale is a function that changes the time of the game
+        // Time.timeScale = 0f => the game is paused and the player cannot play when the 
+        // pause menu is active
         Time.timeScale = 0f;
         isPaused = true;
     }
 
+    /// <summary>
+    /// Shows the confirmation menu
+    /// </summary>
+    /// <remarks>
+    /// It has only one line because a button cannot have 
+    /// an onclick function that returns an IEnumerator
+    /// </remarks>
+    /// <param name="active">The boolean that dictates if the menu should be active or not</param>
     public void Confirmation(bool active) 
     {
-        if (!active)
-        {
-            StartCoroutine(MenuAnimation(transitionConfirmation));
-        }
-
-        confirmationMenu.SetActive(active);
+        StartCoroutine(ConfirmationCoroutine(active));
     }
 
+    /// <summary>
+    /// Shows the confirmation menu
+    /// </summary>
+    /// <remarks>
+    /// The time of the game is scaled back to normal BEFORE the coroutine because
+    /// the WaitForSeconds function waits game time seconds
+    /// </remarks>
     public void QuitGame()
     {
         Time.timeScale = 1f;
         StartCoroutine(fadeScene.LoadScene("MenuScene"));
     }
 
-    public IEnumerator MenuAnimation(Animator transition)
+    /// <summary>
+    /// Handles the pause menu animation
+    /// </summary>
+    /// <remarks>
+    /// The time of the game is scaled back to normal BEFORE the coroutine because
+    /// the WaitForSeconds function waits game time seconds
+    /// Also, the block handling the animation is copy pasted into the function
+    /// ConfirmationCoroutine and not in a function because calling 
+    /// StartCoroutine() doesn't stop the rest of the code
+    /// </remarks>
+    public IEnumerator PauseMenuAnimation()
     {
+        Time.timeScale = 1f;
+        isPaused = false;
+        
         // Start animation
-        transition.SetTrigger("Start");
+        transitionPause.SetTrigger("Start");
         // Wait a bit
-        yield return new WaitForSeconds(1);
+        yield return new WaitForSeconds(0.25f);
+
+        pauseMenu.SetActive(false);
+    }
+
+    /// <summary>
+    /// Handles the confirmation menu animation
+    /// </summary>
+    /// <remarks>
+    /// The time of the game is scaled back to normal BEFORE the coroutine because
+    /// the WaitForSeconds function waits game time seconds
+    /// Also, the block handling the animation is copy pasted into the function
+    /// ConfirmationCoroutine and not in a function because calling 
+    /// StartCoroutine() doesn't stop the rest of the code
+    /// </remarks>
+    /// <param name="active">The boolean that dictates if the menu should be active or not</param>
+    public IEnumerator ConfirmationCoroutine(bool active)
+    {
+        Time.timeScale = 1f;
+        if (!active)
+        {
+            // Start animation
+            transitionConfirmation.SetTrigger("Start");
+            // Wait a bit
+            yield return new WaitForSeconds(0.25f);
+        }
+
+        confirmationMenu.SetActive(active);
+        Time.timeScale = 0f;
     }
 }
