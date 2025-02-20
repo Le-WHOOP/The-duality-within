0cd0186f0649850ec7df3cd185fb48ba672156eb
Magali Gilbert <magali.gilbert@epita.fr>
2025-02-20 17:27:21 +0100

    Add: prefab objects

--- Assets/Scripts/Game/Interactions/InteractableChaos.cs
+++ Assets/Scripts/Game/Interactions/InteractableChaos.cs
@@ -5,10 +5,24 @@ public class InteractableChaos : InteractableController
     [SerializeField]
     private float _chaosValue;
 
+    [SerializeField]
+    [Tooltip("Leave empty if this item doesn't have a flammable interaction")]
+    private GameObject _flameObject;
+
     public InteractableChaos(Personnality availableTo) : base(Personnality.Hyde) { }
 
     public override void Interact(CityPlayerController player)
     {
         ChaosSystem.Instance.RaiseChaos(_chaosValue);
+
+        // Show fire animation
+        _flameObject.SetActive(true);
+        // Wait a bit and deactivate the GameObject
+        Invoke("deactivateGameObject", 0.5f);
+    }
+
+    private void deactivateGameObject()
+    {
+        gameObject.SetActive(false);
     }
 }
